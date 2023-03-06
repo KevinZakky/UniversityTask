1182c0c319df072ac63ba82139401190a63009f3
KevinZakky <125472082+KevinZakky@users.noreply.github.com>
2023-03-06 20:14:28 +0700

    Delete STRUKTUR-DATA/modul-1 directory

--- STRUKTUR-DATA/modul-1/DoubleHasil.java
+++
@@ -1,8 +0,0 @@
-package modul1;
-
-public class DoubleHasil extends Segitiga<Double>{
-
-    public Double getResultAsDouble() {
-      return 0.5 * getAlas() * getTinggi();
-    }
-}
--- STRUKTUR-DATA/modul-1/IntegerHasil.java
+++
@@ -1,8 +0,0 @@
-package modul1;
-
-public class IntegerHasil extends Segitiga<Integer> {
-
-    public Integer getResultAsInt(){
-        return (int) (0.5 * getAlas() * getTinggi());
-    }
-}
