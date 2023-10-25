ab44292e7a4775f668ae452665849298225ee170
KevinZakky <125472082+KevinZakky@users.noreply.github.com>
2023-10-25 21:38:57 +0700

    commit modul3

---
+++ PEMROGRAMAN-WEB/src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <h1>Halaman Contact</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
---
+++ PEMROGRAMAN-WEB/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <h1>Halaman Home</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
